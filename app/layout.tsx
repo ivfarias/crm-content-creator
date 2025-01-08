d31fc9cf8e64a53c6b4f40fad556f73c4a4e46d0
ivfarias <imivanfarias@gmail.com>
2025-01-08 11:07:15 -0300

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import PasswordProtection from "../components/PasswordProtection";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,8 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <PasswordProtection>{children}</PasswordProtection>
       </body>
     </html>
   );
 }
+
